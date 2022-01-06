7031ed9837acff1de30bb317eb8f4ce7cf67c61b
Irfanca1 <irfanchoiruddin23@gmail.com>
2022-01-06 21:44:11 +0700

    mencoba bikin web design e-commerce

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+let menuItems = document.getElementById("menuItems");
+
+menuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if(menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px";
+    } else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
+
+// JS FOR PRODUCT GALLERY
+let productImg = document.getElementById("product-img");
+let smallImg = document.querySelectorAll(".small-img");
+
+smallImg[0].onclick = function() {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    productImg.src = smallImg[3].src;
+}
